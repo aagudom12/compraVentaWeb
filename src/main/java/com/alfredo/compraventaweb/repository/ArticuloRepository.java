20019acf353ebba7952686a17b7532ca5a96c5de
alfredo <agudo.mota1314@gmail.com>
2024-11-13 17:39:52 +0100

    Eliminación de artículo

--- src/main/java/com/alfredo/compraventaweb/repository/ArticuloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alfredo.compraventaweb.repository;
-
-import com.alfredo.compraventaweb.entity.Articulo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticuloRepository extends JpaRepository<Articulo, Long> {
-}
